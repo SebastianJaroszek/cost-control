4afbb9b03f5c3447f9025dc9dab75be5b889de27
Tomasz Rutyna <tomasz.rutyna@altkom.pl>
2018-02-25 20:55:15 +0100

    Basic Spring configuration

---
+++ src/main/java/pl/sda/costcontrol/config/AppConfiguration.java
@@ -0,0 +1,12 @@
+package pl.sda.costcontrol.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author trutyna
+ */
+@Configuration
+@ComponentScan(basePackages = {"pl.sda.costcontrol"})
+public class AppConfiguration {
+}
