e8e7c459de9421f2b3653d79898ecfedffe12113
Tomasz Rutyna <tomasz.rutyna@altkom.pl>
2018-02-25 22:03:07 +0100

    First view

--- src/main/java/pl/sda/costcontrol/config/AppConfiguration.java
+++ src/main/java/pl/sda/costcontrol/config/AppConfiguration.java
@@ -1,12 +1,28 @@
 package pl.sda.costcontrol.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 /**
  * @author trutyna
  */
 @Configuration
 @ComponentScan(basePackages = {"pl.sda.costcontrol"})
+@EnableWebMvc
 public class AppConfiguration {
+
+    @Bean
+    public ViewResolver internalResourceViewResolver() {
+        InternalResourceViewResolver bean = new InternalResourceViewResolver();
+        bean.setViewClass(JstlView.class);
+        bean.setPrefix("/resources/views/");
+        bean.setSuffix(".jsp");
+        return bean;
+    }
+
 }
---
+++ src/main/java/pl/sda/costcontrol/web/MainPageController.java
@@ -0,0 +1,17 @@
+package pl.sda.costcontrol.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author trutyna
+ */
+@Controller
+public class MainPageController {
+
+    @GetMapping(value = "/main")
+    public String mainPage() {
+        return "main";
+    }
+
+}
